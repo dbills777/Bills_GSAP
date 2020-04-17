409675bb2ec5fc601bc51cb36c0e9cf74ae44dd3
dbills777 <dbills777@gmail.com>
2020-04-16 20:57:45 -0600

    test

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
-// Custom JS here
-console.log('this is inside the js file')
+// suble motion for heading text content
+gsap.from(".subtle", {opacity: 0, duration: 5, y: -150, stagger: 0.5})
+gsap.from(".vertical", {opacity: 0, duration: 5, y: 300, x: -150})
+
+
+let preorder = document.querySelector(".animate-btn")
+
+
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+preorder.addEventListener('click', event => {
+  console.log('is it workin');
+  gsap.to(".offscreen",{
+    duration: 5,
+    y: 10,
+    opacity: 1,
+    scale: 0.8
+  }); 
+
+});
+
+
+
